9114317338d8473497c9429c7f7040826e0be5f3
Ebrahim <Ebrahim@DESKTOP-ULBFU9K>
2021-10-13 12:22:28 +0330

    create startup class and set config serilog

--- Asp06Store.ShopUI/Program.cs
+++ Asp06Store.ShopUI/Program.cs
@@ -1,30 +1,41 @@
-using Asp06Store.Core.Domain.Models;
-using Asp06Store.Core.Domain.Models.Products;
-using Microsoft.EntityFrameworkCore;
+using Asp06Store.Infra.Data.Sql.Common;
+using Asp06Store.ShopUI;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+CreateHostBuilder(args).Build().Run();
 
-builder.Services.AddControllersWithViews();
-var cnnString = builder.Configuration.GetConnectionString("StoreCnn");
-builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(cnnString));
-builder.Services.AddScoped<IProductRepository, EfProductRepository>();
-builder.Services.AddMemoryCache();
-builder.Services.AddSession();
-var app = builder.Build();
+static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                }).UseSerilog(LoggingConfiguration.ConfigureLogger);
 
-app.UseDeveloperExceptionPage();
-app.UseStatusCodePages();
-app.UseStaticFiles();
-app.UseSession();
-app.UseRouting();
+//var builder = WebApplication.CreateBuilder(args);
 
-app.UseEndpoints(endpoints =>
-        {
-            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}/Page{PageNumber}");
-            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/Page{PageNumber}");
-            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}");
-            endpoints.MapDefaultControllerRoute();
-        }
-    );
+//builder.Services.AddControllersWithViews();
+//var cnnString = builder.Configuration.GetConnectionString("StoreCnn");
+//builder.Services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(cnnString));
+//builder.Services.AddScoped<IProductRepository, EfProductRepository>();
+//builder.Services.AddMemoryCache();
+//builder.Services.AddSession();
+//var app = builder.Build();
 
-app.Run();
+//app.UseDeveloperExceptionPage();
+//app.UseStatusCodePages();
+//app.UseStaticFiles();
+//app.UseSession();
+//app.UseRouting();
+
+//app.UseSerilogRequestLogging((loggerConfiguration) => LoggingConfiguration.ConfigureLogger(, loggerConfiguration));
+
+//app.UseEndpoints(endpoints =>
+//        {
+//            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}/Page{PageNumber}");
+//            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/Page{PageNumber}");
+//            endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}");
+//            endpoints.MapDefaultControllerRoute();
+//        }
+//    );
+
+//app.Run();
---
+++ Asp06Store.ShopUI/Startup.cs
@@ -0,0 +1,58 @@
+﻿using Asp06Store.Core.Domain.Models;
+using Asp06Store.Core.Domain.Models.Products;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp06Store.ShopUI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+
+
+            services.AddMvc()
+            .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+            services.AddDbContext<StoreDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("StoreCnn")));
+            services.AddScoped<IProductRepository, EfProductRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDeveloperExceptionPage();
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}/Page{PageNumber}");
+                endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/Page{PageNumber}");
+                endpoints.MapControllerRoute("pagination", "/{controller=home}/{action=index}/{category}");
+                endpoints.MapDefaultControllerRoute();
+            });
+
+
+        }
+    }
+}
